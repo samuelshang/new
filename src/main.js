98d42dc84a4186fbccf733bee9cded9148cb0e19
samuelshang <291243897@qq.com>
2017-04-20 07:27:27 +0800

    modi

--- src/main.js
+++ src/main.js
@@ -24,20 +24,6 @@ const router=new VueRouter({
 
 //axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
 //
-axios.interceptors.request.use(function (config) {  //配置发送请求的信息
-  stores.dispatch('showLoading')  
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
-
-axios.interceptors.response.use(function (response) { //配置请求回来的信息
-  stores.dispatch('hideLoading')
-  return response;
-}, function (error) {
-
-  return Promise.reject(error);
-});
 
 /*axios.defaults.baseURL = (process.env.NODE_ENV !=='production' ? config.dev.httpUrl:config.build.httpUrl);
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';*/
